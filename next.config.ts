bbde3cdc930cf3b5da04a9d6437c670e429711a5
Madhav Chaturvedi <madhavchaturvedi0562@gmail.com>
2025-08-23 00:25:54 +0530

    update next.config

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,13 @@
-const umami_url = process.env.NEXT_PUBLIC_UMAMI_WEBSITE_URL ?? '';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['ts', 'tsx'],
   transpilePackages: ['next-mdx-remote'],
+   eslint: {
+    ignoreDuringBuilds: true,
+  },
   async rewrites() {
-    return [
-      {
-        source: '/umami.js',
-        destination: `${umami_url}/script.js`,
-      },
-      {
-        source: '/api/send',
-        destination: `${umami_url}/api/send`,
-      },
-    ];
+    return [];
   },
 };
 
