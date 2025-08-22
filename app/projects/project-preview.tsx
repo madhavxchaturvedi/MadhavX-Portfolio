05379376fe4236de39fed988c7f3b62f3976ad6b
Madhav Chaturvedi <madhavchaturvedi0562@gmail.com>
2025-08-23 00:45:18 +0530

    update project preview

--- app/projects/project-preview.tsx
+++ app/projects/project-preview.tsx
@@ -17,13 +17,13 @@ const scaleAnimation = {
     scale: 1,
     x: '-50%',
     y: '-50%',
-    transition: { duration: 0.4, ease: [0.76, 0, 0.24, 1] },
+    transition: { duration: 0.4, ease: [0.76, 0, 0.24, 1] as any},
   },
   closed: {
     scale: 0,
     x: '-50%',
     y: '-50%',
-    transition: { duration: 0.4, ease: [0.32, 0, 0.67, 0] },
+    transition: { duration: 0.4, ease: [0.32, 0, 0.67, 0] as any},
   },
 };
 
