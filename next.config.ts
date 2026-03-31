8e352cc738fdda8e717a6f2a02f522c887020af6
Madhav Chaturvedi <madhavchaturvedi0562@gmail.com>
2026-03-31 19:49:43 +0530

    chore: update Next.js to version 16.1.7 and adjust TypeScript configuration
    - Updated Next.js dependency from 15.3.8 to ^16.1.7 in package.json.
    - Reformatted the lib array in tsconfig.json for better readability.
    - Changed JSX setting from "preserve" to "react-jsx" in tsconfig.json.
    - Added ".next/dev/types/**/*.ts" to the include paths in tsconfig.json.
    - Reformatted the exclude array in tsconfig.json for consistency.

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['ts', 'tsx'],
   transpilePackages: ['next-mdx-remote'],
-   eslint: {
-    ignoreDuringBuilds: true,
-  },
   async rewrites() {
     return [];
   },
